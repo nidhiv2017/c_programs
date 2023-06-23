b41009ab2b27f0fe9876de4e4ef6fa2e3de9906f
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-06-23 18:26:21 +0530

    Update palindrome_string.c

--- palindrome_string.c
+++ palindrome_string.c
@@ -1,7 +1,23 @@
+//Write a program that takes a sentence as input and prints the longest word in it
 #include <stdio.h>
-int main(void){
-  char string[] = "We are the challengers";
-  int length = sizeof(string)/sizeof(string[0]);
-  
-  return 0;
+
+int main(void) {
+    char string[] = "arora";
+    int isPalindrome = 1;
+    int length = sizeof(string) / sizeof(string[0]);
+
+    for (int i = 0; i < length / 2; i++) {
+        if (string[i] != string[length - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome == 1) {
+        printf("It's a palindrome.\n");
+    } else {
+        printf("It's not a palindrome.\n");
+    }
+
+    return 0;
 }
