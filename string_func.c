16f98af7fc4afecf100cf71c0bda99127e6eec6f
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-06-26 00:56:12 +0530

    update_string_func.c

--- string_func.c
+++ string_func.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-int main(void){
 
+int main(void){
+  char name1[6];
+  char name2[5]; 
+  printf("Enter the first name: ");
+  for(int i = 0; i < 6; i++){
+    scanf(" %c", &name1[i]);
+  }
+  printf("Enter the second name: ");
+  for(int i = 0; i < 5; i++){
+    scanf(" %c", &name2[i]);
+  }
+
+  for(int i = 0; i < 6; i++){
+    printf("%c", name1[i]);
+  }
+  for(int i = 0; i < 5; i++){
+    printf("%c", name2[i]);
+  }
+  
   return 0;
 }
