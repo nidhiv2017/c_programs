26623f0825aa8cc2423eba52872700ebd991e697
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-06-26 23:38:32 +0530

    create_ string_length_strlen.c

---
+++ string_length_strlen.c
@@ -0,0 +1,9 @@
+//length of string using strlen
+#include <stdio.h>
+#include <string.h>
+int main(void){
+  char str1[] = "manish";
+  printf("The length if str1 is %lu", strlen(str1));
+  
+  return 0;
+}
