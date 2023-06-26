7350b1aa141f9e52ece9e32636dd6a848e8df25a
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2023-06-26 23:59:37 +0530

    Create string_comparison_using_strcmp.c

---
+++ string_comparison_using_strcmp.c
@@ -0,0 +1,10 @@
+//string comparison using 
+#include <stdio.h>
+#include <string.h>
+int main(void){
+  char str1[] = "manish";
+  char str2[] = "nidhi";
+  printf("The strcmp for str1, str2 %d", strcmp(str1, str2));
+ 
+  return 0;
+}
